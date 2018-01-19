44143d6e47e32d3eb98a7e5bc3766e3110a55b32
Рувим Кондратьев <ruvim.k.1@gmail.com>
2018-01-19 12:23:11 -0800

    Add a check to canSwipe () to ensure that we don't try to go back when canGoBack () is false

--- app/src/main/java/com/gradgoose/pngannotator/NoteActivity.java
+++ app/src/main/java/com/gradgoose/pngannotator/NoteActivity.java
@@ -868,7 +868,7 @@ public class NoteActivity extends Activity {
 				if (direction < 0) goBack (); 
 			} 
 			@Override public boolean canSwipe (int direction) { 
-				return direction <= 0; 
+				return direction <= 0 && canGoBack (); 
 			} 
 		}); 
 		// Put it into the list only if it's not empty (to avoid a scroll bar problem): 

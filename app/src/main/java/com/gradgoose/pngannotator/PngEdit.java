1d982dc2943d039dfc83d46c0672e26132d1fe30
Рувим Кондратьев <ruvim.k.1@gmail.com>
2018-01-22 16:17:16 -0800

    Try to implement a method to convert a path for stroking into a path for filling; for erasing purposes later

--- app/src/main/java/com/gradgoose/pngannotator/PngEdit.java
+++ app/src/main/java/com/gradgoose/pngannotator/PngEdit.java
@@ -92,6 +92,26 @@ public class PngEdit {
 		} 
 	} 
 	
+	private static final int HALF_CIRCLE_SEGMENT_COUNT = 8; 
+	public static float [] convertPathToPolygons (float path [], float strokeRadius) { 
+		if (path.length < 2) return new float [0]; 
+		float polygons [] = new float [2 * 2 * HALF_CIRCLE_SEGMENT_COUNT]; 
+		for (int segment = 1; 2 * segment < path.length; segment++) { 
+			int basePathIndex = 2 * (segment - 1); 
+			int resultBaseIndex = 4 * HALF_CIRCLE_SEGMENT_COUNT * (segment - 1); 
+			double angle = Math.atan2 (path[basePathIndex + 3] - path[basePathIndex + 1], path[basePathIndex + 2] - path[basePathIndex + 0]); 
+			for (int i = 0; i < HALF_CIRCLE_SEGMENT_COUNT; i++) { 
+				polygons[resultBaseIndex + 2 * i + 0] = (float) (strokeRadius * Math.cos (angle + (.5f + (float) i / HALF_CIRCLE_SEGMENT_COUNT) * Math.PI) + path[basePathIndex + 0]); 
+				polygons[resultBaseIndex + 2 * i + 1] = (float) (strokeRadius * Math.sin (angle + (.5f + (float) i / HALF_CIRCLE_SEGMENT_COUNT) * Math.PI) + path[basePathIndex + 1]); 
+			} 
+			for (int i = 0; i < HALF_CIRCLE_SEGMENT_COUNT; i++) { 
+				polygons[resultBaseIndex + 2 * i + 0] = (float) (strokeRadius + Math.cos (angle + (.5f + (float) i / HALF_CIRCLE_SEGMENT_COUNT) * Math.PI) + path[basePathIndex + 2]); 
+				polygons[resultBaseIndex + 2 * i + 1] = (float) (strokeRadius + Math.sin (angle + (.5f + (float) i / HALF_CIRCLE_SEGMENT_COUNT) * Math.PI) + path[basePathIndex + 3]); 
+			} 
+		} 
+		return polygons; 
+	} 
+	
 	public void erase (float eraserPath [], float eraserRadius) { 
 		if (eraserPath.length < 2) return; 
 		synchronized (mEdits) { 

ac2c445a801515aa9705ee2e0bf728a9bcd7fd66
Рувим Кондратьев <ruvim.k.1@gmail.com>
2018-06-08 17:19:42 -0700

    Format

--- app/src/main/java/com/gradgoose/pennotepad/PngNotesAdapter.java
+++ app/src/main/java/com/gradgoose/pennotepad/PngNotesAdapter.java
@@ -297,7 +297,7 @@ public class PngNotesAdapter extends RecyclerView.Adapter {
 					RedrawParams params = mAllRedrawParams.elementAt (i); 
 					if (params.dirty && params.file == null) { 
 						params.dirty = false; 
-						params.clearTarget ();
+						params.clearTarget (); 
 					} 
 				} 
 				for (int i = 0; i < cnt; i++) { 

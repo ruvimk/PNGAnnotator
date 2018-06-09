9ee172c5ec5fab96b590499d8c2188dbd2505655
Рувим Кондратьев <ruvim.k.1@gmail.com>
2018-06-08 17:21:59 -0700

    Add a nested loop at the beginning to clear images

--- app/src/main/java/com/gradgoose/pennotepad/PngNotesAdapter.java
+++ app/src/main/java/com/gradgoose/pennotepad/PngNotesAdapter.java
@@ -294,13 +294,13 @@ public class PngNotesAdapter extends RecyclerView.Adapter {
 				hasDirty = false; 
 				int cnt = mAllRedrawParams.size (); 
 				for (int i = 0; i < cnt; i++) { 
-					RedrawParams params = mAllRedrawParams.elementAt (i); 
-					if (params.dirty && params.file == null) { 
-						params.dirty = false; 
-						params.clearTarget (); 
+					for (int j = 0; j < cnt; j++) { 
+						RedrawParams params = mAllRedrawParams.elementAt (j); 
+						if (params.dirty && params.file == null) { 
+							params.dirty = false; 
+							params.clearTarget (); 
+						} 
 					} 
-				} 
-				for (int i = 0; i < cnt; i++) { 
 					final RedrawParams params = mAllRedrawParams.elementAt (i); 
 					if (!params.dirty) continue; 
 					if (params.pageView.getWidth () == 0 || params.pageView.getHeight () == 0) 
